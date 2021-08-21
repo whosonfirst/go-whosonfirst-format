265fa07728646aa1546165243387ba52d2c1d4a6
Tom Taylor <tom@tomtaylor.co.uk>
2021-08-21 20:27:44 +0100

    Add wof-format binary

---
+++ bin/wof-format.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	format "github.com/whosonfirst/go-whosonfirst-format"
+)
+
+func main() {
+	flag.Parse()
+
+	var reader *bufio.Reader
+
+	if flag.NArg() == 0 {
+		info, err := os.Stdin.Stat()
+		if err != nil {
+			fmt.Printf("Unable to open stdin: %s", err)
+			os.Exit(1)
+			return
+		}
+
+		if info.Mode()&os.ModeCharDevice != 0 || info.Size() <= 0 {
+			fmt.Println("Usage: cat input.geojson | wof-format > output.geojson")
+			os.Exit(1)
+			return
+		}
+
+		reader = bufio.NewReader(os.Stdin)
+	} else {
+		f, err := os.Open(flag.Arg(0))
+		if err != nil {
+			fmt.Printf("Unable to open file: %s", err)
+			os.Exit(1)
+			return
+		}
+
+		reader = bufio.NewReader(f)
+	}
+
+	inputBytes, err := io.ReadAll(reader)
+	if err != nil {
+		fmt.Printf("Unable to read: %s", err)
+		os.Exit(1)
+		return
+	}
+
+	var feature format.Feature
+	err = json.Unmarshal(inputBytes, &feature)
+	if err != nil {
+		fmt.Printf("Invalid JSON: %s", err)
+		os.Exit(1)
+		return
+	}
+
+	outputBytes, err := format.FormatFeature(&feature)
+	if err != nil {
+		fmt.Printf("Failed to format feature: %s", err)
+		os.Exit(1)
+		return
+	}
+
+	fmt.Printf("%s", outputBytes)
+}
